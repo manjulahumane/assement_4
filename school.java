42369ab3258146879e8034cfbe77720137c073d2
manjulahumane <manjulab.xworkz@gmail.com>
2021-12-20 19:00:18 +0530

    upload file

---
+++ school.java
@@ -0,0 +1,15 @@
+class school{
+public static void main(String[] args){
+
+int[] id={1,2,3,4,5,6,7,8,9,10};
+System.out.println(id[0]);
+System.out.println(id[1]);
+System.out.println(id[2]);
+System.out.println(id[3]);
+System.out.println(id[4]);
+System.out.println(id[5]);
+System.out.println(id[6]);
+System.out.println(id[8]);
+System.out.println(id[9]);
+}
+}
